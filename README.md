f8059fe54cd58856e0cccf35d3cfb21531238bab
ryananger <nightcrawling@gmail.com>
2023-02-07 20:27:01 -0500

    Harmony first commit.

--- README.md
+++
@@ -1,1 +0,0 @@
-## Oververse
--- client/src/components/DevBar.jsx
+++ client/src/components/DevBar.jsx
@@ -6,95 +6,11 @@ import Charmander from './Game/Charmander.js';
 import Player from './Game/Player.js';
 
 var Dev = function({Game}) {
-  var spawnPlayer = function() {
-    if (Game.player) {
-      return;
-    }
-
-    var p = Player(0, 0);
-    var cam = Game.camera;
-
-    Game.player = p;
-    Game.entities.unshift(p);
-    cam.following = p;
-  };
-
-  var spawnBulbasaur = function() {
-    var w = 2000;
-    var h = 2000;
-
-    var x = -w/2 + Math.floor(Math.random() * w);
-    var y = -h/2 + Math.floor(Math.random() * h);
-
-
-    if (Game.camera) {
-      x += Game.camera.x;
-      y += Game.camera.y;
-    }
-
-    var bulbasaur = Bulbasaur(x, y);
-    var entities = Game.entities;
-    var uis = Game.uis;
-
-    entities.unshift(bulbasaur);
-    uis.unshift(bulbasaur.healthBar);
-  };
-
-  var spawnCharmander = function() {
-    var w = 2000;
-    var h = 2000;
-
-    var x = -w/2 + Math.floor(Math.random() * w);
-    var y = -h/2 + Math.floor(Math.random() * h);
-
-
-    if (Game.camera) {
-      x += Game.camera.x;
-      y += Game.camera.y;
-    }
-
-    var charmander = Charmander(x, y);
-    var entities = Game.entities;
-    var uis = Game.uis;
-
-    entities.unshift(charmander);
-    uis.unshift(charmander.healthBar);
-  };
-
-  var spawn100 = function() {
-    for (var i = 0; i < 100; i++) {
-      if (Math.random() < 0.05) {
-        spawnCharmander();
-      } else {
-        spawnBulbasaur();
-      }
-    }
-  };
-
-  var spawn1000 = function() {
-    for (var i = 0; i < 1000; i++) {
-      spawnBulbasaur(true);
-    }
-  };
 
   var clearEntities = function() {
     Game.player = null;
     Game.entities = [Game.camera];
     Game.uis = [];
-  };
-
-  var followPlayer = function() {
-    Game.entities.map(function(ent) {
-      if (ent.isCamera || ent.isPlayer) {
-        return;
-      }
-
-      if (!ent.following) {
-        ent.following = Game.player;
-      } else {
-        ent.following = null;
-      }
-    })
   };
 
   var toggleUI = function() {
@@ -134,16 +50,10 @@ var Dev = function({Game}) {
       </div>
       <div className="devButtons v">
         <button id="toggleUI"      onClick={toggleUI}>        Toggle UI.              </button>
-        <button id="spawnPlayer"   onClick={spawnPlayer}>     Player.                 </button>
-        <button id="spawnEntity"   onClick={spawnBulbasaur}>  Bulbasaur.              </button>
-        <button id="spawnChar"     onClick={spawnCharmander}> Charmander.              </button>
-        <button id="spawn100"      onClick={spawn100}>        Spawn 100.              </button>
-        {/* <button id="spawn1000"     onClick={spawn1000}>       Spawn 1000.             </button> */}
-        <button id="followPlayer"  onClick={followPlayer}>    Follow.                 </button>
         <button id="logState"      onClick={logState}>        Log state.              </button>
         <button id="showBorders"   onClick={toggleBorders}>   Borders.                </button>
         <button id="showBoxes"     onClick={toggleBoxes}>     Boxes.                  </button>
-        {/* <button id="logEntities"   onClick={logEntities}>     Log entities.           </button> */}
+        <button id="logEntities"   onClick={logEntities}>     Log entities.           </button>
         <button id="clearEntities" onClick={clearEntities}>   Clear entities.         </button>
       </div>
     </div>
